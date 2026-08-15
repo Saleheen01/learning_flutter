9766cf861cf9407a92ebc03c946f776f9e317feb
Saleheen Ferdous <mdsaleheenferdous@gmail.com>
2026-08-15 14:06:19 +0600

    Dynamic form generation

--- lib/main.dart
+++ lib/main.dart
@@ -19,43 +19,58 @@ class MyApp  extends StatelessWidget{
 class HomeActivity extends StatelessWidget{
   HomeActivity({super.key});
 
-  void mysnackar (String message, BuildContext context){
-    ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text(message)),
-
-    );
-
+  // void mysnackar (String message, BuildContext context){
+  //   ScaffoldMessenger.of(context).showSnackBar(
+  //       SnackBar(content: Text(message)),
+  //
+  //   );
+  //
+  // }
+  //
+  // MyAlertDialogue(context){
+  //   return showDialog(
+  //       context: context,
+  //       builder: (BuildContext context){
+  //         return Expanded(child: AlertDialog(
+  //           title: Text("Alert"),
+  //           content: Text("Do you want to delete?"),
+  //           actions: [
+  //             TextButton(onPressed: (){
+  //               mysnackar("Delete Success", context);
+  //               Navigator.of(context).pop();
+  //             },
+  //                 child: Text("yes")
+  //             ),
+  //
+  //             TextButton(onPressed: (){
+  //                 mysnackar("Content isn't deleted", context);
+  //                 Navigator.of(context).pop();},
+  //                 child: Text("No")
+  //             ),
+  //           ],
+  //         ));
+  //       }
+  //
+  //   );
+  // }
+  //
+  //
+  // ButtonStyle buttonStyle = ElevatedButton.styleFrom(
+  //     minimumSize: Size(double.infinity, 60),
+  //   backgroundColor: Colors.green,
+  // );
+  var Mylist = [
+    {"img": "https://picsum.photos/500/300", "title": "Saleheen"},
+    {"img": "https://picsum.photos/500/300", "title": "Ferdous"},
+    {"img": "https://picsum.photos/500/300", "title": "Saleh"},
+    {"img": "https://picsum.photos/500/300", "title": "Zim"},
+    {"img": "https://picsum.photos/500/300", "title": "Zannatul"},
+    {"img": "https://picsum.photos/500/300", "title": "Mawa"},
+  ];
+  mysnackbar(context,msg){
+    return ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(msg)));
   }
 
-  MyAlertDialogue(context){
-    return showDialog(
-        context: context,
-        builder: (BuildContext context){
-          return Expanded(child: AlertDialog(
-            title: Text("Alert"),
-            content: Text("Do you want to delete?"),
-            actions: [
-              TextButton(onPressed: (){
-                mysnackar("Delete Success", context);
-                Navigator.of(context).pop();
-              }, child: Text("yes")),
-
-              TextButton(onPressed: (){
-                  mysnackar("Content isn't deleted", context);
-                  Navigator.of(context).pop();},
-                  child: Text("No")),
-            ],
-          ));
-        }
-
-    );
-  }
-
-
-  ButtonStyle buttonStyle = ElevatedButton.styleFrom(
-      minimumSize: Size(double.infinity, 60),
-    backgroundColor: Colors.green,
-  );
   @override
   Widget build(BuildContext context) {
     int currentIndex=0;
@@ -64,222 +79,33 @@ class HomeActivity extends StatelessWidget{
       appBar: AppBar(
         title: Text("Inventory App"),
         backgroundColor: Colors.green,
-        actions: [
-          IconButton(onPressed: (){mysnackar("this is profile", context);},
-              icon: Icon(Icons.person)),
-          IconButton(onPressed: (){mysnackar("this is contacts", context);},
-              icon: Icon(Icons.contacts)),
-          IconButton(onPressed: (){mysnackar("this is email", context);},
-              icon: Icon(Icons.email)),
-
-        ],
+        // actions: [
+        //   IconButton(onPressed: (){mysnackar("this is profile", context);},
+        //       icon: Icon(Icons.person)),
+        //   IconButton(onPressed: (){mysnackar("this is contacts", context);},
+        //       icon: Icon(Icons.contacts)),
+        //   IconButton(onPressed: (){mysnackar("this is email", context);},
+        //       icon: Icon(Icons.email)),
+        //
+        // ],
       ),
+      body: ListView.builder(
+        itemCount: Mylist.length,
+        itemBuilder: (context,index){
+          return GestureDetector(
+            onTap: (){mysnackbar(context, Mylist[index]['title']);},
+            child: Container(
+              margin: EdgeInsets.all(0),
+              width: double.infinity,
+              height: 150,
+              child: Image.network(Mylist[index]['img']!,fit: BoxFit.fill,),
 
-      floatingActionButton: FloatingActionButton(
-        elevation: 10,
-        backgroundColor: Colors.green,
-        onPressed: () {mysnackar("This is floating action button", context);},
-        child: Icon(Icons.add),
-      ),
 
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.green,
-        currentIndex: 0,
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.person),label:"Profile"),
-          BottomNavigationBarItem(icon: Icon(Icons.contacts),label:"Contacts"),
-          BottomNavigationBarItem(icon: Icon(Icons.mail),label:"Email"),
-        ],
-        onTap: (int index)
-        {
-          if(index ==0)
-          {
-            mysnackar("I'm Profile", context);
-          }
-          if(index ==1)
-          {
-            mysnackar("I'm Contacts", context);
-          }if(index ==2)
-          {
-            mysnackar("I'm Email", context);
-          }
+            ),
+          );
         },
       ),
 
-      drawer: Drawer(
-        child: ListView(
-          children: [
-            DrawerHeader(
-              padding:EdgeInsets.all(0),
-              child: UserAccountsDrawerHeader(
-                accountName: Text("Saleheen"),
-                accountEmail: Text("mdsaleheenferdous@gmail.com"),
-                currentAccountPicture: Image.network("https://upload.wikimedia.org/wikipedia/commons/1/17/Google-flutter-logo.png"),
-                onDetailsPressed: (){mysnackar("My all details", context);},
-              ),
-            ),
-            ListTile(
-              leading: Icon(Icons.person),
-              title: Text("Profile"),
-              selected: currentIndex==0,
-              selectedColor: Colors.green,
-              selectedTileColor: Colors.green.shade100,
-              onTap: ()
-              {
-                mysnackar("I'm profile", context);
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.contacts),
-              title: Text("Contacts"),
-              selected: currentIndex==0,
-              selectedColor: Colors.green,
-              selectedTileColor: Colors.green.shade100,
-              onTap: ()
-              {
-                mysnackar("I'm profile", context);
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.email),
-              title: Text("Email"),
-              selected: currentIndex==0,
-              selectedTileColor: Colors.green.shade100,
-              selectedColor: Colors.green,
-              onTap: ()
-              {
-                mysnackar("I'm profile", context);
-              },
-            ),
-          ],
-        ),
-      ),
-
-
-      endDrawer: Drawer(
-        child: ListView(
-          children: [
-            DrawerHeader(
-              padding:EdgeInsets.all(0),
-              child: UserAccountsDrawerHeader(
-                accountName: Text("Saleheen"),
-                accountEmail: Text("mdsaleheenferdous@gmail.com"),
-                currentAccountPicture: Image.network("https://upload.wikimedia.org/wikipedia/commons/1/17/Google-flutter-logo.png"),
-                //onDetailsPressed: (){mysnackar("My all details", context);},
-              ),
-            ),
-            ListTile(
-              leading: Icon(Icons.person),
-              title: Text("Profile"),
-              selected: currentIndex==0,
-              selectedColor: Colors.green,
-              selectedTileColor: Colors.green.shade100,
-              onTap: ()
-              {
-                mysnackar("I'm profile", context);
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.contacts),
-              title: Text("Contacts"),
-              selected: currentIndex==0,
-              selectedColor: Colors.green,
-              selectedTileColor: Colors.green.shade100,
-              onTap: ()
-              {
-                mysnackar("I'm profile", context);
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.email),
-              title: Text("Email"),
-              selected: currentIndex==0,
-              selectedTileColor: Colors.green.shade100,
-              selectedColor: Colors.green,
-              onTap: ()
-              {
-                mysnackar("I'm profile", context);
-              },
-            ),
-          ],
-        ),
-      ),
-      // body: Container(
-      //   width: 450,
-      //   height: 450,
-      //   padding: const EdgeInsets.all(0),
-      //   decoration: BoxDecoration(
-      //     color: Colors.grey,
-      //     border: Border.all(color: Colors.black87, width: 10),
-      //   ),
-      //   child: const Text("Hello Flutter",
-      //     style: TextStyle(color: Colors.white),
-      //     textAlign: TextAlign.center,
-      //   ),
-      // ),
-
-      // body: Column(
-      //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      //   children: [
-      //     Container(
-      //       height: 50,
-      //       width: 100,
-      //       child: Image.network("https://upload.wikimedia.org/wikipedia/commons/1/17/Google-flutter-logo.png"),
-      //     ),
-      //     Container(
-      //       height: 50,
-      //       width: 100,
-      //       child: Image.network("https://upload.wikimedia.org/wikipedia/commons/1/17/Google-flutter-logo.png"),
-      //     ),
-      //     Container(
-      //       height: 50,
-      //       width: 100,
-      //       child: Image.network("https://upload.wikimedia.org/wikipedia/commons/1/17/Google-flutter-logo.png"),
-      //     ),
-      //   ],
-      // ),
-      // body: Row(
-      //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      //   children: [
-      //     Container(
-      //       height: 50,
-      //       width: 100,
-      //       child: Image.network("https://upload.wikimedia.org/wikipedia/commons/1/17/Google-flutter-logo.png"),
-      //     ),
-      //     Container(
-      //       height: 50,
-      //       width: 100,
-      //       child: Image.network("https://upload.wikimedia.org/wikipedia/commons/1/17/Google-flutter-logo.png"),
-      //     ),
-      //     Container(
-      //       height: 50,
-      //       width: 100,
-      //       child: Image.network("https://upload.wikimedia.org/wikipedia/commons/1/17/Google-flutter-logo.png"),
-      //     ),
-      //   ],
-      // ),
-      // body: Row(
-      //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      //   children: [
-      //     TextButton(onPressed: (){mysnackar("Im text Button", context);}, child: Text("Text_Button")),
-      //     OutlinedButton(onPressed: (){mysnackar("Im Out_Line Button", context);}, child: Text("out_Line__Button")),
-      //     ElevatedButton(onPressed: (){MyAlertDialogue(context);}, child: Text("Click Me"),style: buttonStyle,)
-      //     // ElevatedButton(onPressed: (){mysnackar("Im Elevated Button", context);}, child: Text("elevated_Button"),style: buttonStyle,),
-      //   ],
-      //
-      // ),
-
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Padding(padding: EdgeInsets.all(10),child: TextField(decoration: InputDecoration(border: OutlineInputBorder(),label: Text("First Name")),),),
-          Padding(padding: EdgeInsets.all(10),child: TextField(decoration: InputDecoration(border: OutlineInputBorder(),label: Text("Last Name")),),),
-          Padding(padding: EdgeInsets.all(10),child: TextField(decoration: InputDecoration(border: OutlineInputBorder(),label: Text("Input Password")),),),
-          Padding(padding: EdgeInsets.all(10),child: ElevatedButton(onPressed: (){MyAlertDialogue(context);}, child: Text("Submit"),style: buttonStyle,)),
-
-
-        ],
-      ),
     );
   }
 
