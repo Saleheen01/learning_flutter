430939ead54923e66263ace5ac8f472c7f38777b
Saleheen Ferdous <mdsaleheenferdous@gmail.com>
2026-08-17 02:43:13 +0600

    Card Implementation

--- lib/main.dart
+++ lib/main.dart
@@ -20,105 +20,29 @@ class MyApp  extends StatelessWidget{
 class HomeActivity extends StatelessWidget{
   HomeActivity({super.key});
 
-  mysnackbar(context,msg){
-    return ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(msg)));
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
        appBar: AppBar(
          title: Text("MyApp",style: TextStyle(color: Colors.white),),
-         backgroundColor: Colors.black,
+         backgroundColor: Colors.blue,
        ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          ElevatedButton(onPressed: (){
-            Navigator.push(context, MaterialPageRoute(builder: (context)=>Activity1("This is home to Activity_1")));
-
-          },
-            style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blue,),
-            child: Text("Go to Activity_1",style: TextStyle(color: Colors.black),),
-          ),
-
-          ElevatedButton(onPressed: (){
-            Navigator.push(context, MaterialPageRoute(builder: (context)=>Activity2("This is home to Activity_2")));
-
-          },
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.blue,),
-            child: Text("Go to Activity_2",style: TextStyle(color: Colors.black),),
-          ),
-
-
-        ],
-      ),
-    );
-  }
-}
-
-
-
-class Activity1 extends StatelessWidget{
-  String msg;
-   Activity1(
-      this.msg,
-      {super.key}) {
-
-  }
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(msg),
-      ),
       body: Center(
-        child: ElevatedButton(
-            onPressed: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context)=>Activity2("This is Activity_1 to Activity_2")));
-            },
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.green,),
-            child: Text("Go to Activity_2",
-              style: TextStyle(
-                  color: Colors.black,
-                  ),
-            )
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(100),
+          ),
+          shadowColor: Colors.green,
+          color: Colors.green,
+          elevation: 80,
+          child: SizedBox(
+            height: 200,
+            width: 200,
+            child: Center(child: Text("This is card"),),
+          ),
         ),
       ),
     );
   }
 }
 
-
-class Activity2 extends StatelessWidget{
-  String msg;
-  Activity2(
-      this.msg,
-      {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(msg),
-      ),
-      body: Center(
-        child: ElevatedButton(
-            onPressed: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context)=>Activity2("This is Activity_2 to Activity_1")));
-            },
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.green,),
-            child: Text("Go to Activity_1",
-              style: TextStyle(
-                color: Colors.black,
-              ),
-            )
-        ),
-      ),
-    );
-  }
-}
